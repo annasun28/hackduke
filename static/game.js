5e5991272215955a1adb77d53104933474facd7e
rachelbransom <rachelbransom@yahoo.com>
2015-11-07 14:52:14 -0500

    game.html

---
+++ static/game.js
@@ -0,0 +1,23 @@
+/**
+ * Created by rachelbransom on 11/7/15.
+ */
+
+var canvas = document.createElement("canvas");
+
+var ctx = canvas.getContext("2d");
+
+canvas.width = 512;
+canvas.height = 480;
+
+document.body.appendChild(canvas);
+
+// background image
+
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+    bgReady = true;
+
+};
+
+bgImage.src =
